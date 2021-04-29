10f8c7d1b4394d7e50882a8f28fd6b47e99dba93
jpritha <aripritha1998@gmail.com>
2021-04-30 00:06:53 +0530

    third commit

---
+++ Javascript Project Task/ProjectTask_9/main.js
@@ -0,0 +1,20 @@
+let registered = false;
+var runner_age= 50
+let raceNumber = Math.floor(Math.random() * 1000);
+
+
+if(runner_age>=18 && registered===true){
+    racenumber+=1000;
+}
+if(runner_age>18 && registered===false){
+    console.log(`race at 9:30 am ${racenumber}`)
+}
+else if(runner_age>18 && registered){
+    console.log(`race at 11:00 am ${racenumber}`)
+}
+else if(runner_age<18){
+    console.log(`race at 12:30 am ${racenumber}`)
+}
+else{
+    console.log(`see the registration desk`)
+}
